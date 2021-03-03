0c385d327535f9d504f9558d5a14856b5235cf8c
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-03 16:43:56 +0530

    values and variables

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,15 @@
+////////////////////////////////////
+// Linking a JavaScript File
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+////////////////////////////////////
+// Values and Variables
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
