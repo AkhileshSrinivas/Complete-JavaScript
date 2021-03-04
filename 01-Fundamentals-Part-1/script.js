c66af24c642a66960c59ffba9cbb145651046e3f
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-04 21:16:21 +0530

    Logical Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -273,7 +273,7 @@ if (height) {
     console.log('Height is UNDEFINED');
 }
 
-*/
+
 
 
 ////////////////////////////////////
@@ -309,4 +309,30 @@ if (age >= 18) {
     alert(`Oops!, Sorry you have ${18 - age} years left to get driving license`)
 } else {
     alert("Oops!, Sorry your not allowed to driving license")
+}
+
+*/
+
+////////////////////////////////////
+// Logical Operators
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log('Sarah is able to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...');
 }
