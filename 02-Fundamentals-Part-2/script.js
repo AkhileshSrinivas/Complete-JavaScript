10853a30f6ce937bbf48c4e81807ed2d7c4d011b
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-08 21:48:03 +0530

    Arrow functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -36,7 +36,7 @@ console.log(appleOrangeJuice);
 
 const num = Number('23');
 
-*/
+
 
 ///////////////////////////////////////
 // Function Declarations vs. Expressions
@@ -54,3 +54,21 @@ const calcAge2 = function (birthYeah) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+*/
+
+///////////////////////////////////////
+// Arrow functions
+
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+    const age = 2037 - birthYeah;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
