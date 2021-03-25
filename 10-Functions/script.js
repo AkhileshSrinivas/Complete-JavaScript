4b6c75b1c48bf44d438945ae39ff0386eb34646f
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-25 12:57:15 +0530

    Functions Returning Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -66,7 +66,7 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 
-*/
+
 
 ///////////////////////////////////////
 // Functions Accepting Callback Functions
@@ -111,3 +111,24 @@ const high5 = function () {
 };
 document.body.addEventListener('click', high5);
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+*/
+
+///////////////////////////////////////
+// Functions Returning Functions
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+// Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Jonas');
