ad8488cda8ce830f2eaa7138692e1d30802afe18
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-25 12:46:01 +0530

    Functions Accepting Callback Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -30,7 +30,7 @@ createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
 
-*/
+
 
 ///////////////////////////////////////
 // How Passing Arguments Works: Values vs. Reference
@@ -65,3 +65,49 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+
+*/
+
+///////////////////////////////////////
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+
+const string1 = function () {
+  console.log(
+    'This is to inform you that you clicked on a buy new plane button'
+  );
+};
+const string2 = function () {
+  console.log(
+    'This is to inform you that you clicked on a Answer poll !? button'
+  );
+};
+
+document.querySelector('.buy').addEventListener('click', string1);
+document.querySelector('.poll').addEventListener('click', string2);
+
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
