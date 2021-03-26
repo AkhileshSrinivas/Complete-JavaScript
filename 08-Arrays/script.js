6099d497509fc24188c20fe0a964447510834aff
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-26 19:14:49 +0530

    The Magic of Chaining Methods

--- 08-Arrays/script.js
+++ 08-Arrays/script.js
@@ -178,6 +178,8 @@ console.log(withdrawals);
 
 ///////////////////////////////////////
 // The reduce Method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 console.log(movements);
 
 // accumulator -> SNOWBALL
@@ -220,6 +222,7 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
+/*
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   const adults = humanAges.filter(age => age >= 18);
@@ -240,3 +243,23 @@ const calcAverageHumanAge = function (ages) {
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
+
+*/
+
+///////////////////////////////////////
+// The Magic of Chaining Methods
+
+const eurToUsd = 1.1;
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
