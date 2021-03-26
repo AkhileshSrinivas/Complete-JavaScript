51e85f185fe0dc98b438fc078b2b0d4a43a60581
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-26 16:30:58 +0530

    The filter Method

--- 08-Arrays/script.js
+++ 08-Arrays/script.js
@@ -129,7 +129,7 @@ const checkDogs = function (dogsJulia, dogsKate) {
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
-*/
+
 
 ///////////////////////////////////////
 // The map Method
@@ -156,3 +156,21 @@ const movementsDescriptions = movements.map(
     )}`
 );
 console.log(movementsDescriptions);
+
+*/
+
+///////////////////////////////////////
+// The filter Method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
