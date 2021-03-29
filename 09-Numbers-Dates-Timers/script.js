36c60b396d2701fadd7e6b3b386bdc457ada1979
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-29 18:20:10 +0530

    Timers

--- 09-Numbers-Dates-Timers/script.js
+++ 09-Numbers-Dates-Timers/script.js
@@ -242,7 +242,7 @@ const calcDaysPassed = (date1, date2) =>
 const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
 console.log(days1);
 
-*/
+
 
 ///////////////////////////////////////
 // Internationalizing Numbers (Intl)
@@ -262,3 +262,25 @@ console.log(
   navigator.language,
   new Intl.NumberFormat(navigator.language, options).format(num)
 );
+
+*/
+
+///////////////////////////////////////
+// Timers
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 1000);
