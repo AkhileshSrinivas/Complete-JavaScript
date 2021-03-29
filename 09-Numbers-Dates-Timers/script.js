eb4d24542241df6ae6c0045e0d64a30fb5338f63
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-03-29 17:17:06 +0530

    Operations With Dates

--- 09-Numbers-Dates-Timers/script.js
+++ 09-Numbers-Dates-Timers/script.js
@@ -148,7 +148,6 @@ console.log(huge + ' is REALLY big!!!');
 console.log(11n / 3n);
 console.log(10 / 3);
 
-*/
 
 ///////////////////////////////////////
 // Creating Dates
@@ -229,3 +228,16 @@ console.log(Date.now());
 
 future.setFullYear(2040);
 console.log(future);
+
+*/
+
+///////////////////////////////////////
+// Operations With Dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
