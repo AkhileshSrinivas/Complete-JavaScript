8b40037189e4ad317a1e8de9557e79fdc2ea90b5
AKHILESH .S <akhileshsrinivas.nr5@gmail.com>
2021-04-01 12:27:57 +0530

    Prototypal Inheritance on Built-In Objects

--- 11-OOPs/script.js
+++ 11-OOPs/script.js
@@ -57,3 +57,36 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__.__proto__);
+console.log(Object.prototype.isPrototypeOf(jonas.__proto__));
+
+Object.prototype.can = function () {
+  return 'this is posible';
+};
+
+console.log(jonas.can());
+
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
